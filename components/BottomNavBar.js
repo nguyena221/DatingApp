e22310e6cff5bafde280270d3bd0d339e15613cc
Annie Nguyen <nguyenannie221@gmail.com>
2025-06-25 15:45:12 -0700

    changed navbar

--- components/BottomNavBar.js
+++ components/BottomNavBar.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import HomeScreen from "../screens/HomeScreen";
 import MessagesScreen from "../screens/MessagesScreen";
 import ProfileScreen from "../screens/ProfileScreen";
-//import { CurrentRenderContext } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomNavBar() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
+        tabBarShowLabel: false,
         tabBarStyle: styles.tabBar,
-        tabBarLabelStyle: styles.tabLabel,
-      }}
+        tabBarItemStyle: styles.tabBarItem,
+        tabBarIconStyle: styles.tabBarIcon,
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Messages") {
+            iconName = focused ? "chatbubble" : "chatbubble-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
+          }
+
+          return (
+            <View style={styles.iconWrapper}>
+              <Ionicons name={iconName} size={26} color={color} />
+            </View>
+          );
+        },
+        tabBarActiveTintColor: "#333",
+        tabBarInactiveTintColor: "#bbb",
+      })}
     >
       <Tab.Screen name="Messages" component={MessagesScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
@@ -28,8 +49,8 @@ const styles = StyleSheet.create({
   tabBar: {
     position: "absolute",
     bottom: 30,
-    width: 300,
-    height: 70,
+    width: 200,
+    height: 60, // Reduced height
     backgroundColor: "#fff",
     borderRadius: 35,
     borderTopWidth: 0,
@@ -39,10 +60,20 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     elevation: 5,
     left: "50%",
-    marginLeft: 60, // im still looking at other ways to center the navbar
+    marginLeft: 100,
   },
-  tabLabel: {
-    fontSize: 14,
-    fontWeight: "600",
+  tabBarItem: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    height: "100%",
+  },
+  tabBarIcon: {
+    marginBottom: 0,
+  },
+  iconWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
   },
 });
